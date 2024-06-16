503f16da47ff87195668d0976b8c271ddfae1ed1
BridgetKas <bridgetnamugga@gmail.com>
2024-06-16 20:45:56 +0900

    Added Exclusive Component

--- src/components/exclusive/exclusive.jsx
+++ src/components/exclusive/exclusive.jsx
@@ -1,13 +1,29 @@
+import ButtonComp from '../common/Button';
 import styles from './exclusive.module.css'
-import dealsImage from '../../assets/exclusive.jpg'
+import { colors } from "../../utilis/color"
+import { borderRadius } from '../../utilis/theme';
+
 
 function ExclusiveComp() {
   return (
     <div>
-      <p>fkfkfk</p>
       <div className={styles.imageContainer}>
-        <img src={dealsImage} alt="Shoe Deals"/>
+        <h1>Exclusive Hot Deal Ends Soon</h1>
+        <p className={styles.description}>Who are in extremely love with eco friendly system</p>
+        <div className={styles.dateContainer}>
+          <div><span id="days"></span> Days</div>
+          <div><span id="hours"></span> Hours</div>
+          <div><span id="minutes"></span> Minutes</div>
+          <div><span id="seconds"></span> Seconds</div>
+        </div>
+        <ButtonComp variant="contained" 
+          size="medium" 
+          btnTitle="Shop"  
+          style={{ background: colors.yellowGrad, position:"relative", zIndex:"2"}} 
+          onClick={() => alert('Clicked')}
+        />
       </div>
+
     </div>
   )
 }
