ec8a57409b740de09b084481c9413753675a8938
BridgetKas <bridgetnamugga@gmail.com>
2024-06-26 20:31:58 +0900

    Added category Component

--- src/pages/Shop/category.jsx
+++ src/pages/Shop/category.jsx
@@ -19,48 +19,48 @@ function CategoryComp() {
         <li><a>Baby care</a> (53)</li>
       </ul>
       <div>
-        <div>Product Filters</div>
-        <div>Brands</div>
-        <div>
+        <div className={styles.browse}>Product Filters</div>
+        <div className={styles.brand}>Brands</div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="apple"  value="apple" />
           <label htmlFor="apple">Apple(29)</label>
         </div>
-        <div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="asus"  value="asus" />
           <label htmlFor="asus">Asus(29)</label>
         </div>
-        <div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="gionee"  value="gionee"  />
           <label htmlFor="gionee">Gionee(19)</label>
         </div>
-        <div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="micromax"  value="micromax" />
           <label htmlFor="micromax">Micromax(19)</label>
         </div>
-        <div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="samusung"  value="samusung" />
           <label htmlFor="samusung">Samusung(19)</label>
         </div>
       </div>
       <div>
-        <div>Color</div>
-        <div>
+        <div className={styles.brand}>Color</div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="black"  value="black" />
           <label htmlFor="black">Black(29)</label>
         </div>
-        <div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="blackleather"  value="BL" />
           <label htmlFor="BL">Black Leather(29)</label>
         </div>
-        <div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="Black with red"  value="BWR"  />
           <label htmlFor="BWR">Black with Red(19)</label>
         </div>
-        <div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="micromax"  value="micromax" />
           <label htmlFor="micromax">Gold(19)</label>
         </div>
-        <div>
+        <div className={styles.labelContainer}>
           <input type="radio" id="samusung"  value="samusung" />
           <label htmlFor="samusung">Spacegrey(19)</label>
         </div>
