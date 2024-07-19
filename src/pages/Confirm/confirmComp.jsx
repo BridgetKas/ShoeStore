de7ca4cdf5816a3f60edbf7620539c9139097c54
BridgetKas <bridgetnamugga@gmail.com>
2024-07-19 20:44:00 +0900

    Added Confirmation  Page

--- src/pages/Confirm/confirmComp.jsx
+++ src/pages/Confirm/confirmComp.jsx
@@ -7,61 +7,63 @@ function ConfirmComp() {
       <HeaderComp title='Checkout' firstpage='first' nextpage='next'/>
       <div>
         <h3 className={styles.order}>Thank you. Your order has been received</h3>
-        <div className={styles.orderContainer}>
-          <p className={styles.mainTitle}>Order Info</p>
-          <div className={styles.container}>
-            <p className={styles.title}>Order number</p>
-            <p className={styles.description}>:60235</p>
+        <div className={styles.mainorderContainer}>
+          <div className={styles.orderContainer}>
+            <p className={styles.mainTitle}>Order Info</p>
+            <div className={styles.container}>
+              <p className={styles.title}>Order number</p>
+              <p className={styles.description}>:60235</p>
+            </div>
+            <div className={styles.container}>
+              <p className={styles.title}>Date </p>
+              <p className={styles.description}>:Los Angeles</p>
+            </div>
+            <div className={styles.container}>
+              <p className={styles.title}>Total</p>
+              <p className={styles.description}>:USD 2210</p>
+            </div>
+            <div className={styles.container}>
+              <p className={styles.title}>Payment method</p>
+              <p className={styles.description}>:Check payment</p>
+            </div>
           </div>
-          <div className={styles.container}>
-            <p className={styles.title}>Date </p>
-            <p className={styles.description}>:Los Angeles</p>
+          <div className={styles.orderContainer}>
+            <p className={styles.mainTitle}>Billing Address</p>
+            <div className={styles.container}>
+              <p className={styles.title}>Street</p>
+              <p className={styles.description}>:58/6</p>
+            </div>
+            <div className={styles.container}>
+              <p className={styles.title}>City</p>
+              <p className={styles.description}>:Los Angeles</p>
+            </div>
+            <div className={styles.container}>
+              <p className={styles.title}>Country</p>
+              <p className={styles.description}>:United States</p>
+            </div>
+            <div className={styles.container}>
+              <p className={styles.title}>Postcode</p>
+              <p className={styles.description}>:36952</p>
+            </div>
           </div>
-          <div className={styles.container}>
-            <p className={styles.title}>Total</p>
-            <p className={styles.description}>:USD 2210</p>
-          </div>
-          <div className={styles.container}>
-            <p className={styles.title}>Payment method</p>
-            <p className={styles.description}>:Check payment</p>
-          </div>
-        </div>
-        <div className={styles.orderContainer}>
-          <p className={styles.mainTitle}>Billing Address</p>
-          <div className={styles.container}>
-            <p className={styles.title}>Street</p>
-            <p className={styles.description}>:58/6</p>
-          </div>
-          <div className={styles.container}>
-            <p className={styles.title}>City</p>
-            <p className={styles.description}>:Los Angeles</p>
-          </div>
-          <div className={styles.container}>
-            <p className={styles.title}>Country</p>
-            <p className={styles.description}>:United States</p>
-          </div>
-          <div className={styles.container}>
-            <p className={styles.title}>Postcode</p>
-            <p className={styles.description}>:36952</p>
-          </div>
-        </div>
-        <div className={styles.orderContainer}>
-          <p className={styles.mainTitle}>Shipping Address</p>
-          <div className={styles.container}>
-            <p className={styles.title}>Street</p>
-            <p className={styles.description}>:58/6</p>
-          </div>
-          <div className={styles.container}>
-            <p className={styles.title}>City</p>
-            <p className={styles.description}>:Los Angeles</p>
-          </div>
-          <div className={styles.container}>
-            <p className={styles.title}>Country</p>
-            <p className={styles.description}>:United States</p>
-          </div>
-          <div className={styles.container}>
-            <p className={styles.title}>Postcode</p>
-            <p className={styles.description}>:36952</p>
+          <div className={styles.orderContainer}>
+            <p className={styles.mainTitle}>Shipping Address</p>
+            <div className={styles.container}>
+              <p className={styles.title}>Street</p>
+              <p className={styles.description}>:58/6</p>
+            </div>
+            <div className={styles.container}>
+              <p className={styles.title}>City</p>
+              <p className={styles.description}>:Los Angeles</p>
+            </div>
+            <div className={styles.container}>
+              <p className={styles.title}>Country</p>
+              <p className={styles.description}>:United States</p>
+            </div>
+            <div className={styles.container}>
+              <p className={styles.title}>Postcode</p>
+              <p className={styles.description}>:36952</p>
+            </div>
           </div>
         </div>
       </div>
