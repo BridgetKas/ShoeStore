bf8ebbc52868f43a15842a76b301c85c6f9bad85
BridgetKas <bridgetnamugga@gmail.com>
2024-07-23 21:15:53 +0900

    Fixed Cart Page

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,5 +1,7 @@
 import styles from './cart.module.css'
 import imageCart from '../../assets/cart.jpg'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 function CartItemComp() {
   return (
@@ -7,6 +9,7 @@ function CartItemComp() {
       <div className={styles.container}>
         <div className={styles.mainCartContainer}>
           <div className={styles.imageCartContainer}>
+
             <div>
               <img src={imageCart} alt="image of the item purchased" />
             </div>
@@ -14,7 +17,14 @@ function CartItemComp() {
           </div>
           <div className={styles.priceCartContainer}>
             <p className={styles.priceCart}>$700</p>
-            <input type="number" value='1' className={styles.inputCart}/>
+            <div className={styles.mainNumberContainer}>
+              <input type="number" value='1' className={styles.inputCart}/>
+              <div className={styles.numberContainer}>
+                <KeyboardArrowUpIcon color="primary"/>
+                <KeyboardArrowDownIcon color="primary"/>
+              </div>
+            </div>
+          
             <p className={styles.priceCart}>$2300</p>
           </div>
         </div>
--- src/pages/Cart/cartPage.jsx
+++ src/pages/Cart/cartPage.jsx
@@ -2,36 +2,57 @@ import CartItemComp from '../../components/cart/cart'
 import ButtonComp from '../../components/common/Button'
 import HeaderComp from '../../layout/Header/header'
 import styles from './cartPage.module.css'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 function CartPageComp() {
   return (
     <div>
       <HeaderComp title='Shopping Cart' firstpage='first' nextpage='next'/>
-      <CartItemComp/>
-      <CartItemComp/>
-      <CartItemComp/>
+      <div className={styles.mainContainer}>
+        <CartItemComp/>
+        <CartItemComp/>
+        <CartItemComp/>
 
-      <div className={styles.maincashContainer}>
-        <div className={styles.cashContainer}>
-          <div>
-            <ButtonComp btnTitle="UPDATE CART"/>
+        <div className={styles.maincashContainer}>
+
+          <div className={styles.cashContainer}>
+            <div>
+              <ButtonComp btnTitle="UPDATE CART"/>
+            </div>
+            <div className={styles.btnContainer}>
+              <ButtonComp btnTitle="Coupon code" />
+              <ButtonComp btnTitle="APPLY"/>
+              <ButtonComp btnTitle="CLOSE COUPON"/>
+            </div>
           </div>
-          <div className={styles.btnContainer}>
-            <ButtonComp btnTitle="Coupon code" />
-            <ButtonComp btnTitle="APPLY"/>
-            <ButtonComp btnTitle="CLOSE COUPON"/>
-          </div>
-        </div>
-        <div className={styles.priceContainer}>
-          <div className={styles.cash}></div>
-          <div>
+          <div className={styles.priceContainer}>
             <p>Subtotal</p>
             <p>$2160.00</p>
           </div>
-        
+          <div className={styles.rateContainer}>
+            <div className={styles.rate}>
+              <label >Shipping Flat rate: $50.00</label>
+              <input type="radio"  value="rate"/>
+            </div>
+            <div className={styles.rate}>
+              <label >Free shipping </label>
+              <input type="radio"  value="rate"/>
+            </div>
+            <div className={styles.rate}>
+              <label >Flat rate: $10.00 </label>
+              <input type="radio"  value="rate"/>
+            </div>
+            <div className={styles.rate}>
+              <label >Local Delivery </label>
+              <input type="radio"  value="rate"/>
+            </div>
+            <div className={styles.rate}>
+              <p >Calculate Shipping </p>
+              <ArrowDropDownIcon/>
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
   )
 }
