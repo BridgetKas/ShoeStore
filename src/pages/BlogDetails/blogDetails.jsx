10fe131b3616c87b4d2ed2d4a0343ee97861118c
BridgetKas <bridgetnamugga@gmail.com>
2024-07-30 16:40:58 +0900

    Added Blog Details Page

--- src/pages/BlogDetails/blogDetails.jsx
+++ src/pages/BlogDetails/blogDetails.jsx
@@ -7,6 +7,13 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import RedditIcon from '@mui/icons-material/Reddit';
+
+
+
 
 
 function BlogDetails() {
@@ -40,6 +47,13 @@ function BlogDetails() {
                   <p>06 Comments</p>
                   <ChatBubbleOutlineIcon color='primary'/>
                 </div>
+                <div className={styles.iconContainer}>
+                  <FacebookIcon color='primary' fontSize='small'/>
+                  <TwitterIcon color='primary' fontSize='small'/>
+                  <GitHubIcon color='primary' fontSize='small'/>
+                  <RedditIcon color='primary' fontSize='small'/>
+
+                </div>
               </div>
             </div>
             <div>
@@ -48,11 +62,15 @@ function BlogDetails() {
                 MCSE boot camps have its supporters and its detractors. 
                 Some people do not understand why you should have to 
                 spend money on boot camp when you can get the MCSE study materials yourself at a fraction.
+              </p>
+              <p className={styles.blogDescription}>
                 Boot camps have its supporters and its detractors. 
                 Some people do not understand why you should have to spend money on boot camp when you can get 
                 the MCSE study materials yourself at a fraction of the camp price. 
                 However, who has the willpower to actually sit through a self-imposed MCSE training. 
                 who has the willpower to actually sit through a self-imposed.
+              </p>
+              <p className={styles.blogDescription}>
                 Boot camps have its supporters and its detractors. 
                 Some people do not understand why you should have to spend money on boot camp 
                 when you can get the MCSE study materials yourself at a fraction of the camp price. 
